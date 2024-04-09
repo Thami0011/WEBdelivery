d0b2b955149679f7a329fd6eda605c46cdc2984a
Thami <thamielmoumeni@gmail.com>
2024-04-09 16:25:57 +0000

    Initialising frontend and backend projetcs

---
+++ BackEnd_WEBdelivery/src/main/java/ma/emsi/backend_webdelivery/BackEndWeBdeliveryApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.backend_webdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndWeBdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndWeBdeliveryApplication.class, args);
+	}
+
+}
---
+++ BackEnd_WEBdelivery/src/test/java/ma/emsi/backend_webdelivery/BackEndWeBdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.backend_webdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndWeBdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
